c2463639f223314f050e98c743b79683cd46ba89
Robert Thompson <robb@robbthompson.co.uk>
2023-12-05 23:10:01 +0000

    up to but not done props

---
+++ part 1/part1/src/App.jsx
@@ -0,0 +1,21 @@
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  )
+}
+
+export default App
