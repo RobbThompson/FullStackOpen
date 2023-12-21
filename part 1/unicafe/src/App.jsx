2d0328ec7f9b7125aeffcb0598a2622aa1fb6b8b
Robert Thompson <robb@robbthompson.co.uk>
2023-12-21 19:14:35 +0000

    Added Components for static elements (titles)

--- part 1/unicafe/src/App.jsx
+++ part 1/unicafe/src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from "react"
 
+const Title = (props) => {
+  console.log(props)
+  return <div>{props.text}</div>
+}
+
+const Stats = (props) => {
+  console.log(props)
+  return <div>{props.text}</div>
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  return <div>code here</div>
+  return (
+    <div>
+      <Title text="Give Feedback" />
+      <Stats text="Statistics" />
+    </div>
+  )
 }
 
 export default App
