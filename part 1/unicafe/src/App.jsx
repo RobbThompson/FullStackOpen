a5847b72c636df273c3a97b50a95c89e1feccc21
Robert Thompson <robb@robbthompson.co.uk>
2023-12-21 19:05:30 +0000

    Exercise 1.6 template

--- part 1/unicafe/src/App.jsx
+++ part 1/unicafe/src/App.jsx
@@ -1,52 +1,12 @@
-import { useState } from 'react'
+import { useState } from "react"
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
-
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-    
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
-
-  return (
-    <div>
-      {left}
-      <Button handleClick={handleLeftClick}  text = 'left'/>
-      <Button handleClick={handleRightClick} text = 'right'/>
-      {right}
-
-      <History allClicks={allClicks} />
-    </div>
-  )
+  return <div>code here</div>
 }
 
 export default App
